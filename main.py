b845f26fb190cba2c6155b8693af3d1c1871bc11
JoeBar3 <149241965+JoeBar3@users.noreply.github.com>
2024-08-30 14:31:52 +0100

    Got file encryption working based on the master password

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import secrets
 import string
+import base64
+import os
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 
 def passwordGen():
@@ -20,4 +25,27 @@ def ispunctuation(char):
     return False
 
 
-print(passwordGen())
+def setUp():
+    masterPass = input("Enter your master password: ")
+    masterPass = masterPass.encode()
+    salt = os.urandom(16)
+    with open("salt.txt", "wb") as f:
+        f.write(salt)
+    print(salt)
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=48000,
+    )
+    encryptionkey = base64.urlsafe_b64encode(kdf.derive(masterPass))
+    f = Fernet(encryptionkey)
+    encryptedContent = f.encrypt(b"Empty")
+    with open("passwords.txt", "wb") as f:
+        f.write(encryptedContent)
+
+
+setUp()
+with open("salt.txt", "rb") as f:
+    salt = f.read()
+    print(salt)
